b66d14c57a03cea5e79dd29cd8833cf5b0dbb038
Daniel <djperrefort@gmail.com>
2020-07-09 11:40:44 -0400

    Removes subtraction functions from referance star module

--- sn_analysis/reference.py
+++ sn_analysis/reference.py
@@ -12,18 +12,12 @@ import astropy.io.fits as fits
 import numpy as np
 import pandas as pd
 
-
 _PARENT = Path(__file__).resolve()
 _DATA_DIR = _PARENT.parent.parent / 'data'
 _STELLAR_SPECTRA_DIR = _DATA_DIR / 'stellar_spectra'
 _SELLAR_FLUX_DIR = _DATA_DIR / 'stellar_fluxes'
 
 available_types = sorted(f.stem for f in _SELLAR_FLUX_DIR.glob('*.txt'))
-
-
-###############################################################################
-# Data Parsing and interpolation
-###############################################################################
 
 
 def _read_stellar_spectra_path(fpath):
@@ -159,10 +153,6 @@ def interp_norm_mag(band, pwv, reference_type='G2'):
     return -2.5 * np.log10(interp_norm_flux(band, pwv, reference_type))
 
 
-###############################################################################
-# Subtracting reference magnitudes from various data types
-###############################################################################
-
 def divide_ref_from_lc(lc_table, pwv, reference_type='G2'):
     """Divide reference flux from a light-curve
 
@@ -181,91 +171,3 @@ def divide_ref_from_lc(lc_table, pwv, reference_type='G2'):
         table_copy['flux'][table_copy['band'] == band] /= ref_flux
 
     return table_copy
-
-
-def subtract_ref_star_array(band, mag, pwv, reference_type='G2'):
-    """Subtract reference star magnitudes from an array of magnitudes
-
-    ``pwv_arr`` should be one dimension less than ``norm_mag``
-
-    Args:
-        band           (str): Name of the band to subtract magnitudes for
-        mag        (ndarray): One or 2d array of magnitudes to subtract from
-        pwv    (float, list): PWV values to get magnitudes for
-        reference_type (str): Type of reference to use (Default 'G2')
-
-   Returns:
-       An array of delta magnitude values
-   """
-
-    if np.ndim(mag) - np.ndim(pwv) != 1:
-        raise ValueError('``pwv`` should be one dimension less than ``norm_mag``')
-
-    ref_mag = interp_norm_mag(band, pwv, reference_type)
-    if np.ndim(ref_mag > 0):
-        ref_mag = ref_mag[:, None]
-
-    return mag - ref_mag
-
-
-def subtract_ref_star_dict(norm_mag, pwv, reference_type='G2'):
-    """Determine magnitude relative to a reference star
-
-    Given magnitudes are expected to be normalized relative to the fiducial
-    atmosphere.
-
-    Args:
-        norm_mag      (dict): Dictionary with arrays of magnitudes for each band
-        pwv (float, ndarray): PWV values for each value in norm_mag
-        reference_type (str): Type of reference star (Default 'G2')
-
-    Return:
-        Dictionary with arrays of magnitudes relative to a reference star for each band
-    """
-
-    # Determine normalized magnitude with respect to reference star
-    return {band: subtract_ref_star_array(band, norm_mag[band], pwv, reference_type) for band in norm_mag}
-
-
-def _subtract_ref_star_slope(band, mag_slope, pwv_config, reference_type='G2'):
-    """Determine (delta magnitude) / (delta pwv) relative to a reference star
-
-    This function separated from ``subtract_ref_star_slope`` for easier testing
-
-    Args:
-        band           (str): Name of the bandpass
-        mag_slope  (ndarray): 1d array of slope values
-        pwv_config    (dict): Config dictionary for fiducial atmosphere
-        reference_type (str): Type of reference star (Default 'G2')
-
-    Return:
-        Dictionary with magnitudes relative to a reference star in each band
-    """
-
-    # Parse reference pwv values
-    pwv_slope_start = pwv_config['slope_start']
-    pwv_slope_end = pwv_config['slope_end']
-
-    # Determine slope in normalized magnitude with respect to reference star
-    mag_slope_start, mag_slope_end = interp_norm_mag(
-        band, [pwv_slope_start, pwv_slope_end], reference_type)
-
-    delta_x = pwv_slope_end - pwv_slope_start
-    stellar_delta_y = mag_slope_end - mag_slope_start
-    norm_mag_delta_y = mag_slope * delta_x
-    return (norm_mag_delta_y - stellar_delta_y) / delta_x
-
-
-def subtract_ref_star_slope(mag_slope, pwv_config, reference_type='G2'):
-    """Determine (delta magnitude) / (delta pwv) relative to a reference star
-
-    Args:
-        mag_slope  (ndarray): 1d array of slope values
-        pwv_config    (dict): Config dictionary for fiducial atmosphere
-        reference_type (str): Type of reference star (Default 'G2')
-
-    Return:
-        Dictionary with slopes relative to a reference star in each band
-    """
-
-    return {b: _subtract_ref_star_slope(b, mag_slope[b], pwv_config, reference_type) for b in mag_slope}
