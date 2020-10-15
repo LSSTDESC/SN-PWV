4edb18a4e1852b222661888ab2f0fb608ab1b1d3
Daniel <djperrefort@gmail.com>
2020-10-14 21:05:28 -0400

    Cleans up conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,10 +9,10 @@
 import sys
 from pathlib import Path
 
+import guzzle_sphinx_theme
+
 package_source_path = Path(__file__).resolve().parent.parent.parent
 sys.path.insert(0, str(package_source_path))
-
-import guzzle_sphinx_theme
 
 # -- Project information -----------------------------------------------------
 
@@ -22,9 +22,7 @@ author = 'DESC'
 
 # -- General configuration ---------------------------------------------------
 
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
+# Add any Sphinx extension module names here, as strings.
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.coverage',
@@ -37,33 +35,24 @@ extensions = [
     'nbsphinx_link'
 ]
 
-pygments_style = 'sphinx'  # Syntax highlighting style
+# Syntax highlighting style
+pygments_style = 'sphinx'
 highlight_language = 'python3'
 
-# Setting for rendering jupyter notebooks
+# Settings for rendering jupyter notebooks
 nbsphinx_execute = 'never'
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 html_static_path = ['./static/']
 
-
 # These paths are either relative to html_static_path
 # or fully qualified paths (eg. https://...)
 html_css_files = [
-    'css/custom.css',
+    'custom_style.css',
 ]
 
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
-
 # -- Options for HTML output -------------------------------------------------
-
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
 
 html_theme_path = guzzle_sphinx_theme.html_theme_path()
 html_theme = 'guzzle_sphinx_theme'
@@ -72,13 +61,3 @@ html_short_title = "SN-PWV"
 html_sidebars = {
     '**': ['logo-text.html', 'searchbox.html', 'globaltoc.html']
 }
-
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-# html_static_path = ['static']
-
-# Add custom css code
-def setup(app):
-    app.add_css_file('custom_style.css')
