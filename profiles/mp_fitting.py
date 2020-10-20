98a780ab9ef74b75f4880be2b97148f6b139b3ef
Daniel <djperrefort@gmail.com>
2020-10-20 15:08:31 -0400

    Makes simulation of light-curves parallel

--- profiles/mp_fitting.py
+++ profiles/mp_fitting.py
@@ -5,19 +5,17 @@
 and then fitting them with a given SN model.
 
 The processing pipeline is as follows:
-  light-curve generator worker
-  -> input Queue
-  -> Pool of fitters
-  -> output Queue
-  -> write to file worker
+  x- WORKER: Load plasticc light-curves -> Queue
+  -> POOL: Retrieve Plastic light-curves and add atmospheric effects -> Queue
+  -> POOL: Fit duplicated light-curves and determine fitted parameters -> Queue
+  -x WORKER: Write fitted parameters to file
 """
 
 import multiprocessing as mp
 import sys
 from copy import copy
-from itertools import repeat
 from pathlib import Path
-from typing import Union, Generator
+from typing import Union
 
 import sncosmo
 from astropy.table import Table
@@ -26,7 +24,6 @@ sys.path.insert(0, '../')
 from snat_sim import modeling, plasticc, filters
 
 model_type = Union[sncosmo.Model, modeling.Model]
-
 filters.register_lsst_filters()
 
 OUT_PATH = Path(__file__).resolve().parent / 'fit_results.csv'
@@ -34,50 +31,94 @@ CADENCE = 'alt_sched'
 
 
 class FittingPipeline:
-    """Parallelized implementation of light-curve fitting"""
-
-    def __init__(self, light_curves: Generator, max_queue=25):
+    def __init__(
+            self,
+            cadence: str,
+            sim_model: model_type,
+            fit_model: model_type,
+            vparams: list,
+            gain: int = 20,
+            skynr: int = 100,
+            quality_callback: callable = None,
+            max_queue=25):
         """Fit light-curves using multiple processes and combine results into an output file
 
         Args:
-            light_curves: Collection of light-curves to iterate over
-        """
-
-        # Defining maximum number of light-curves in each queue is
-        # essential for memory management
-        self.in_queue = mp.Queue(max_queue)
-        self.out_queue = mp.Queue(max_queue)
-
-        self.light_curves = light_curves
-        self.keep_running = True
-
-    def _load_input_queue(self) -> None:
-        """Load light-curves into the inout queue for processing"""
-
-        for lc in self.light_curves:
-            self.in_queue.put(lc)
-
-        self.keep_running = False
-
-    def _fit_lc_wrapper(self, args):
-        self._fit_lc_wrapper(*args)
-
-    def _fit_light_curves(self, model: model_type, vparams: list, **kwargs) -> None:
-        """Fit light-curves using the given model
-
-        Light-curves are retrieved from the input queue, fitted, and then
-        fit results are loaded into the output queue
-
-        Args:
-            model: Model to fit light-curves with
+            cadence: Cadence to use when simulating light-curves
+            sim_model: Model to use when simulating light-curves
+            fit_model: Model to use when fitting light-curves
             vparams: List of parameter names to vary in the fit
-            **kwargs: Any other arguments for ``sncosmo.fit_lc``
+            gain: Gain to use during simulation
+            skynr: Simulate skynoise by scaling plasticc ``SKY_SIG`` by 1 / skynr
+            quality_callback: Skip light-curves if this function returns False
+            max_queue: Maximum number of light-curves to store in memory at once
         """
 
-        model = copy(model)
+        self.cadence = cadence
+        self.sim_model = sim_model
+        self.fit_model = fit_model
+        self.vparams = vparams
+        self.gain = gain
+        self.skynr = skynr
+        self.quality_callback = quality_callback
 
-        while self.keep_running or not self.in_queue.empty():
-            lc = self.in_queue.get()
+        manager = mp.Manager()
+        self.queue_plasticc_lc = manager.Queue(max_queue // 2)
+        self.queue_duplicated_lc = manager.Queue(max_queue // 2)
+        self.queue_fit_results = manager.Queue()
+        self.keep_running = True
+        self.out_path = None  # To be set when ``run`` is called
+
+    def _load_queue_plasticc_lc(self) -> None:
+        """Load light-curves from a given PLaSTICC cadence"""
+
+        # The queue will block the for loop when it is fool, limiting our memory usage
+        for light_curve in plasticc.iter_lc_for_cadence_model(self.cadence, model=11, verbose=True):
+            print('reading', light_curve.meta['SNID'])
+
+            self.queue_plasticc_lc.put(light_curve)
+
+    def _duplicate_light_curves(self) -> None:
+        """Simulate light-curves for a given PLaSTICC cadence"""
+
+        # Determine redshift limit of the given model
+        u_band_low = sncosmo.get_bandpass('lsst_hardware_u').minwave()
+        source_low = self.sim_model.source.minwave()
+        z_limit = (u_band_low / source_low) - 1
+
+        # Skip the light-curve if it is outside the redshift range
+        light_curve = self.queue_plasticc_lc.get()
+        print('duplicating', light_curve.meta['SNID'])
+
+        if light_curve.meta['SIM_REDSHIFT_CMB'] >= z_limit:
+            print('dropping', light_curve.meta['SNID'])
+            return
+
+        # Simulate a duplicate light-curve with atmospheric effects
+        self.sim_model.set(ra=light_curve.meta['RA'], dec=light_curve.meta['DECL'])
+        duplicated_lc = plasticc.duplicate_plasticc_sncosmo(
+            light_curve, self.sim_model, gain=self.gain, skynr=self.skynr)
+
+        # Skip if duplicated light-curve is not up to quality standards
+        if self.quality_callback and not self.quality_callback(duplicated_lc):
+            print('dropping', light_curve.meta['SNID'])
+            return
+
+        self.queue_duplicated_lc.put(duplicated_lc)
+
+    def _fit_light_curves_mp_wrapper(self, args) -> None:
+        """Wrapper for ``_fit_light_curves`` with a signature suitable for a processing pool"""
+
+        self._fit_light_curves()
+
+    def _fit_light_curves(self) -> None:
+        """Fit light-curves using the given model"""
+
+        fit_model = copy(self.fit_model)
+
+        while self.keep_running or not self.queue_duplicated_lc.empty():
+            lc = self.queue_duplicated_lc.get()
+            print('fitting', lc.meta['SNID'])
             out_vals = list(lc.meta.values)
 
             # Use the true light-curve parameters as the initial guess
@@ -85,93 +126,53 @@ class FittingPipeline:
             lc.meta.pop('res', None)
 
             # Fit the model without PWV
-            model.update(lc.meta)
-            _, fitted_model = sncosmo.fit_lc(lc, model, vparams, **kwargs)
+            fit_model.update(lc.meta)
+            _, fitted_model = sncosmo.fit_lc(lc, fit_model, self.vparams)
 
             out_vals.extend(fitted_model.parameters)
-            self.out_queue.put(out_vals)
+            self.queue_fit_results.put(out_vals)
 
     def _unload_output_queue(self) -> None:
         """Retrieve fit results from the output queue and write results to file"""
 
-        with self.out_path.open() as outfile:
-            while self.keep_running or not self.out_queue.empty():
-                new_line = ','.join(self.out_queue.get()) + '\n'
+        with self.out_path.open('w') as outfile:
+            while self.keep_running or not self.queue_fit_results.empty():
+                new_line = ','.join(self.queue_fit_results.get()) + '\n'
                 outfile.write(new_line)
 
-    def run(self, out_path: Path, model: model_type, vparams: list) -> None:
+    def run(self, out_path: Path) -> None:
         """Run fits of each light-curve and write results to file
 
         A ``.csv`` extension is enforced on the output file.
 
         Args:
             out_path: Path to write results to
-            model: Model to fit light-curves with
-            vparams: List of parameter names to vary in the fit
         """
 
         out_path.parent.mkdir(exist_ok=True, parents=True)
         self.out_path = out_path.with_suffix('.csv')
 
-        load_data_process = mp.Process(target=self._load_input_queue)
-        load_data_process.start()
-        load_data_process.join()
+        processes = []
+        pool_size = mp.cpu_count()
 
-        fitting_pool = mp.Pool()
-        fitting_pool.starmap(self._fit_lc_wrapper, repeat((model, vparams)))
-        fitting_pool.join()
+        load_plasticc_process = mp.Process(target=self._load_queue_plasticc_lc)
+        load_plasticc_process.start()
+        processes.append(load_plasticc_process)
+
+        for _ in range(pool_size):
+            duplicate_lc_process = mp.Process(target=self._duplicate_light_curves)
+            duplicate_lc_process.start()
+            processes.append(duplicate_lc_process)
+
+            fitting_process = mp.Process(target=self._fit_light_curves_mp_wrapper)
+            processes.append(fitting_process)
 
         unload_results_process = mp.Process(target=self._unload_output_queue)
         unload_results_process.start()
-        unload_results_process.join()
+        processes.append(unload_results_process)
 
-        load_data_process.close()
-        fitting_pool.close()
-        unload_results_process.close()
-
-
-def iter_custom_lcs(
-        model: model_type,
-        cadence: str,
-        iter_lim: int = None,
-        gain: int = 20,
-        skynr: int = 100,
-        quality_callback: callable = None,
-        verbose: bool = True):
-    """Simulate light-curves for a given PLaSTICC cadence
-
-    Args:
-        model: Model to use in the simulations
-        cadence: Cadence to use when simulating light-curves
-        iter_lim: Stop iteration after given number of light-curves
-        gain: Gain to use during simulation
-        skynr: Simulate skynoise by scaling plasticc ``SKY_SIG`` by 1 / skynr
-        quality_callback: Skip light-curves if this function returns False
-        verbose: Display a progress bar
-    """
-
-    # Determine redshift limit of the given model
-    u_band_low = sncosmo.get_bandpass('lsst_hardware_u').minwave()
-    source_low = model.source.minwave()
-    zlim = (u_band_low / source_low) - 1
-
-    counter = -1
-    iter_lim = float('inf') if iter_lim is None else iter_lim
-    for light_curve in plasticc.iter_lc_for_cadence_model(cadence, model=11, verbose=verbose):
-        counter += 1
-        if counter >= iter_lim:
-            break
-
-        if light_curve.meta['SIM_REDSHIFT_CMB'] >= zlim:
-            continue
-
-        model.set(ra=light_curve.meta['RA'], dec=light_curve.meta['DECL'])
-        duplicated_lc = plasticc.duplicate_plasticc_sncosmo(light_curve, model, gain=gain, skynr=skynr)
-
-        if quality_callback and not quality_callback(duplicated_lc):
-            continue
-
-        yield duplicated_lc
+        for p in processes:
+            p.join()
 
 
 def passes_quality_cuts(light_curve: Table) -> bool:
@@ -203,18 +204,18 @@ if __name__ == '__main__':
     variable_pwv_effect.set(res=5)
 
     # Build a model with atmospheric effects
-    sn_model_with_pwv = modeling.Model(
+    model_with_pwv = modeling.Model(
         source='salt2-extended',
         effects=[variable_pwv_effect],
         effect_names=[''],
         effect_frames=['obs']
     )
 
-    light_curve_generator = iter_custom_lcs(
-        sn_model_with_pwv, cadence=CADENCE, iter_lim=100, quality_callback=passes_quality_cuts)
+    model_without_pwv = sncosmo.Model('Salt2-extended')
 
-    FittingPipeline(light_curve_generator).run(
-        out_path=OUT_PATH,
-        model=sncosmo.Model('Salt2-extended'),
+    FittingPipeline(
+        cadence='alt_sched',
+        sim_model=model_with_pwv,
+        fit_model=model_without_pwv,
         vparams=['x0', 'x1', 'c'],
-    )
+    ).run(out_path=OUT_PATH)
