57338c8fc709a5a935238fb74d26bb9bb13c5a82
Daniel <djperrefort@gmail.com>
2020-10-27 14:15:55 -0400

    Adds tests for index_series_by_seconds function

--- tests/test_weather.py
+++ tests/test_weather.py
@@ -39,7 +39,24 @@ class DatetimeToSecInYear(TestCase):
         return_for_pandas = weather.datetime_to_sec_in_year(pd.to_datetime(self.test_date))
         self.assertEqual(return_for_pandas, return_for_datetime)
 
+
+class IndexSeriesBySeconds(TestCase):
+    """Tests for the ``index_series_by_seconds`` function"""
+
+    def setUp(self):
+        """Create a series indexed by datetime values"""
+
+        data = range(1, 11)
+        datetimes = [datetime(2020, 1, d) for d in data]
+        self.test_series = pd.Series(data, index=datetimes)
+
+    def test_returned_index_is_in_seconds(self):
+        """Compare index of returned series against return from ``datetime_to_sec_in_year``"""
+
+        returned_series = weather.index_series_by_seconds(self.test_series)
+        expected_index = weather.datetime_to_sec_in_year(self.test_series.index)
+        pd.testing.assert_series_equal(returned_series.index, expected_index)
+
 # Todo:
 # weather.supplemented_data()
-# weather.index_series_by_seconds()
 # weather.build_pwv_model()
