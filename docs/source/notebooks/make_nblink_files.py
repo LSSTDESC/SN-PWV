f172ed79757f5e6807a818566e700275ee8e8abf
Daniel Perrefort <djperrefort@pitt.edu>
2020-11-04 12:53:40 -0500

    Fixes bad file name in linke files

--- docs/source/notebooks/make_nblink_files.py
+++ docs/source/notebooks/make_nblink_files.py
@@ -12,4 +12,4 @@ if __name__ == '__main__':
     for file in nb_dir.glob('*.ipynb'):
         with (file_dir / file.with_suffix('.nblink').name).open('w') as new_file:
             print(file_dir / file.with_suffix('.nblink').name)
-            new_file.write(f'{{"path": "../../../notebooks/{file.stem}"}}')
+            new_file.write(f'{{"path": "../../../notebooks/{file.name}"}}')
