3591961c08a28462fb42695f5412bc900b03c5b1
Daniel Perrefort <djperrefort@pitt.edu>
2020-11-12 16:13:32 -0500

    Adds dedicated tests for atmospheric transmission model

--- tests/test_models.py
+++ tests/test_models.py
@@ -5,6 +5,7 @@ from copy import copy
 from unittest import TestCase, skip
 
 import numpy as np
+import pandas as pd
 import sncosmo
 from pwv_kpno.defaults import v1_transmission
 from sncosmo.tests import test_models as sncosmo_test_models
@@ -15,6 +16,52 @@ from snat_sim.filters import register_decam_filters
 from tests.mock import create_constant_pwv_model
 
 register_decam_filters(force=True)
+
+
+class TestFixedResTransmission(TestCase):
+    """Tests for the ``FixedResTransmission`` class"""
+
+    def setUp(self):
+        """Create a dummy ``Transmission`` object"""
+
+        self.transmission = models.FixedResTransmission(4)
+
+    def test_interpolation_on_grid_point(self):
+        """Test interpolation result matches sampled values at the grid points"""
+
+        test_pwv = self.transmission.samp_pwv[1]
+        expected_transmission = self.transmission.samp_transmission[1]
+
+        returned_trans = self.transmission(test_pwv)
+        np.testing.assert_equal(expected_transmission, returned_trans)
+
+    def test_default_wavelengths_match_sampled_wavelengths(self):
+        """Test return values are index by sample wavelengths by default"""
+
+        np.testing.assert_equal(self.transmission(4).index.to_numpy(), self.transmission.samp_wave)
+
+    def test_interpolates_for_given_wavelengths(self):
+        """Test an interpolation is performed for specified wavelengths when given"""
+
+        test_pwv = self.transmission.samp_pwv[1]
+        test_wave = np.arange(3000, 3500, 50)
+
+        returned_wave = self.transmission(test_pwv, wave=test_wave).index.values
+        np.testing.assert_equal(returned_wave, test_wave)
+
+    def test_scalar_pwv_returns_series(self):
+        """Test passing a scalar PWV value returns a pandas Series object"""
+
+        transmission = self.transmission(4)
+        self.assertIsInstance(transmission, pd.Series)
+        self.assertEqual(transmission.name, f'4.0 mm')
+
+    def test_vector_pwv_returns_dataframe(self):
+        """Test passing a vector of PWV values returns a pandas DataFrame"""
+
+        transmission = self.transmission([4, 5])
+        self.assertIsInstance(transmission, pd.DataFrame)
+        np.testing.assert_equal(transmission.columns.values, [f'4.0 mm', f'5.0 mm'])
 
 
 class TestVariablePropagationEffect(TestCase):
