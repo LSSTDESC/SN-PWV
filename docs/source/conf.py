898be9b57d0f74d22abe863b20e6867ca2a98a4c
Daniel Perrefort <djperrefort@pitt.edu>
2020-11-17 16:49:00 -0500

    Makes init docstring render on autoclass

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -62,3 +62,13 @@ html_short_title = 'SNAT-SIM'
 html_sidebars = {
     '**': ['logo-text.html', 'searchbox.html', 'globaltoc.html']
 }
+
+
+def skip(app, what, name, obj, would_skip, options):
+    if name == "__init__":
+        return False
+    return would_skip
+
+
+def setup(app):
+    app.connect("autodoc-skip-member", skip)
