bfb7578d22ce2e44e723f83b7d45b8366d835b51
Daniel Perrefort <djperrefort@pitt.edu>
2020-11-19 10:03:59 -0500

    Adds setup.py file

---
+++ setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
+import re
+from pathlib import Path
+
+from setuptools import find_packages, setup
+
+# Get list of requirements
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+# Get package version
+init_path = Path(__file__).resolve().parent / 'snat_sim/__init__.py'
+with init_path.open('r') as f:
+    s = f.read()
+
+versionRegExp = re.compile("__version__ = '(.*?)'")
+__version__ = versionRegExp.findall(s)[0]
+
+setup(name='snat_sim',
+      version=__version__,
+      packages=find_packages(),
+      keywords='Supernova Atmosphere Chromatic Effects',
+      description='Simulation tools for atmospheric effects in photometric supernova observations.',
+      # long_description=long_description,
+      # long_description_content_type='text/markdown',
+      author='Daniel Perrefort',
+      author_email='djperrefort@pitt.edu',
+      url='https://desc-sn-pwv.readthedocs.io/en/latest/',
+      license='GPL v3',
+      python_requires='>=3.8',
+      install_requires=requirements,
+      include_package_data=True
+      )
