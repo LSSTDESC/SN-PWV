a2c4895826919d659873c5e2f63f23ce8d0513c4
Daniel <djperrefort@gmail.com>
2020-11-27 13:20:24 -0500

    Adds configurable memory limit to memoization wrapper

--- snat_sim/cache_utils.py
+++ snat_sim/cache_utils.py
@@ -5,21 +5,54 @@ Module API
 ----------
 """
 
+import sys
+from collections import OrderedDict
 from functools import wraps
 
 
-def fast_cache(*args):
+class MemoryCache(OrderedDict):
+
+    def __init__(self, max_size=None):
+        """Ordered dictionary with an imposed size limit im memory
+
+        When memory usage exceeds the predefined amount, remove the oldest
+        entry from the cache.
+
+        Args:
+            max_size (int): Maximum memory size in bytes
+        """
+
+        super(MemoryCache, self).__init__()
+        self.max_size = max_size
+        if self.max_size and self.max_size <= (min_size := sys.getsizeof(self)):
+            raise RuntimeError(f'Dictionary size limit must exceed {min_size} bytes')
+
+    def __setitem__(self, key, value):
+        OrderedDict.__setitem__(self, key, value)
+        self._check_size_limit()
+
+    def _check_size_limit(self):
+        if self.max_size is not None:
+            while sys.getsizeof(self) > self.max_size:
+                self.popitem(last=False)
+
+
+def fast_cache(*args, cache_size=None):
     """Memoization decorator supporting ``numpy`` arrays
 
+    Wrapped functions do not accept positional arguments and must use keyword
+    arguments only.
+
     Args:
-        *args (str): Function arguments to treat as numpy arrays
+        *args      (str): Function arguments to treat as numpy arrays
+        cache_size (int): Maximum memory to allocate to cached data in bytes
 
     Returns:
         A callable function decorator
     """
 
     def decorator(function):
-        class Memoization(dict):
+        class Memoization(MemoryCache):
             @wraps(function)
             def wrapper(self, **kwargs):
                 kwargs_for_key = kwargs.copy()
@@ -33,6 +66,6 @@ def fast_cache(*args):
 
                 return out
 
-        return Memoization().wrapper
+        return Memoization(max_size=cache_size).wrapper
 
     return decorator
