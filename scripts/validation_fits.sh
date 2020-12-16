deb1c24b71f42ab168881b1794ff231477cd6720
Daniel Perrefort <djperrefort@pitt.edu>
2020-12-16 14:02:28 -0500

    Updates validation script for new CLI

--- scripts/validation_fits.sh
+++ scripts/validation_fits.sh
@@ -1,4 +1,4 @@
 export CADENCE_SIMS='/home/djperrefort/Github/SN-PWV/scripts/profiling/data'
-python fitting_cli.py -c alt_sched -s 0 -f 0 -o validation/pwv_sim_0_fit_0.csv -i 100 -p 12
-python fitting_cli.py -c alt_sched -s 4 -f 4 -o validation/pwv_sim_4_fit_4.csv -i 100 -p 12
-python fitting_cli.py -c alt_sched -s epoch -f 4 -o validation/pwv_sim_epoch_fit_4.csv -i 100 -p 12
+python fitting_cli.py -s 4 -f 6 -i 100 -o validation/pwv_sim_0_fit_0.csv -c alt_sched --sim_variability 0 --fit_variability 0
+python fitting_cli.py -s 4 -f 6 -i 100 -o validation/pwv_sim_4_fit_4.csv -c alt_sched --sim_variability 4 --fit_variability 4
+python fitting_cli.py -s 4 -f 6 -i 100 -o validation/pwv_sim_epoch_fit_4.csv -c alt_sched --sim_variability epoch --fit_variability 4
