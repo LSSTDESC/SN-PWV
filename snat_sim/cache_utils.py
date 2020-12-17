e2b2ab9ada610487dbbbe9ab872f0af079da1abc
Daniel Perrefort <djperrefort@pitt.edu>
2020-12-17 14:20:38 -0500

    Adds type hints to cache_utils

--- snat_sim/cache_utils.py
+++ snat_sim/cache_utils.py
@@ -9,6 +9,7 @@ import inspect
 import sys
 from collections import OrderedDict
 from functools import wraps
+from typing import *
 
 import numpy as np
 
@@ -16,14 +17,14 @@ import numpy as np
 class MemoryCache(OrderedDict):
     """Ordered dictionary with an imposed limit on overall memory usage"""
 
-    def __init__(self, max_size=None):
+    def __init__(self, max_size: int = None):
         """Ordered dictionary with an imposed size limit im memory
 
         When memory usage exceeds the predefined amount, remove the oldest
         entry from the cache.
 
         Args:
-            max_size (int): Maximum memory size in bytes
+            max_size: Maximum memory size in bytes
         """
 
         super(MemoryCache, self).__init__()
@@ -31,33 +32,33 @@ class MemoryCache(OrderedDict):
         if self.max_size and self.max_size <= (min_size := sys.getsizeof(self)):
             raise RuntimeError(f'Dictionary size limit must exceed {min_size} bytes')
 
-    def __setitem__(self, key, value):
+    def __setitem__(self, key: Hashable, value: Any):
         OrderedDict.__setitem__(self, key, value)
         self._check_size_limit()
 
-    def _check_size_limit(self):
+    def _check_size_limit(self) -> None:
         if self.max_size is not None:
             while sys.getsizeof(self) > self.max_size:
                 self.popitem(last=False)
 
 
-def numpy_cache(*numpy_args, cache_size=None):
+def numpy_cache(*numpy_args: str, cache_size: int = None):
     """Memoization decorator supporting ``numpy`` arrays
 
     Args:
-        *numpy_args (str): Function arguments to treat as numpy arrays
-        cache_size  (int): Maximum memory to allocate to cached data in bytes
+        *numpy_args: Function arguments to treat as numpy arrays
+        cache_size: Maximum memory to allocate to cached data in bytes
 
     Returns:
         A callable function decorator
     """
 
-    def decorator(function):
+    def decorator(function: Callable):
         class Memoization(MemoryCache):
             """Dictionary like object that stores recent function calls in memory"""
 
             @wraps(function)
-            def wrapped(self, *args, **kwargs):
+            def wrapped(self, *args: Any, **kwargs: Any) -> Any:
                 """Wrapped version of the given function
 
                 Arguments and returns are the same as ``function``
