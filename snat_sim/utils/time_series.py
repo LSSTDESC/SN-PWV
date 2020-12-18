4a06b06fb3bbfa5c898508d551964cddde7fe4aa
Daniel Perrefort <djperrefort@pitt.edu>
2020-12-17 20:56:22 -0500

    Makes time series utilities a pandas accessor

--- snat_sim/utils/time_series.py
+++ snat_sim/utils/time_series.py
@@ -7,8 +7,8 @@ Module Docs
 -----------
 """
 
+import datetime as dt
 import warnings
-from datetime import date, datetime
 from typing import *
 
 import numpy as np
@@ -16,17 +16,7 @@ import pandas as pd
 from astropy import units as u
 
 
-@overload
-def datetime_to_sec_in_year(date: datetime) -> float:
-    ...
-
-
-@overload
-def datetime_to_sec_in_year(date: Collection[datetime]) -> np.ndarray:
-    ...
-
-
-def datetime_to_sec_in_year(date):
+def datetime_to_sec_in_year(date: Union[dt.datetime, Collection[dt.datetime]]) -> Union[float, np.ndarray]:
     """Calculate number of seconds elapsed modulo 1 year.
 
     Accurate to within a microsecond.
@@ -35,7 +25,7 @@ def datetime_to_sec_in_year(date):
         date: Date(s) to calculate seconds for
 
     Returns:
-        A single float or a numpy array
+        A single float if the input is a single datetime, or a numpy array if the input is a collection.
     """
 
     # Using ``atleast_1d`` with ``to_datetime`` guarantees a ``DatetimeIndex``
@@ -59,106 +49,14 @@ def datetime_to_sec_in_year(date):
     return seconds
 
 
-def supplemented_data(input_data: pd.Series, year: int, supp_years: Collection[int] = tuple()) -> pd.Series:
-    """Return the supplemented subset of a dataframe corresponding to a given year
-
-    Data for the given year is supplemented with any available data from
-    supplementary years by asserting that the measured values from
-    supplementary years are exactly the same as they would be if taken during
-    the primary year. Priority is given to supplementary years in the order
-    specified by the ``supp_years`` argument.
-
-    Args:
-        input_data: Series of data to use indexed by datetime
-        year: Year to supplement data for
-        supp_years: Years to supplement data with when missing from ``year``
-
-    Returns:
-        A pandas Series object
-    """
-
-    input_data = input_data.dropna().sort_index()
-    years = np.array([year, *supp_years])
-
-    # Check for years with no available data
-    missing_years = years[~np.isin(years, input_data.index.year)]
-    if missing_years:
-        raise ValueError(f'No data for years: {missing_years}')
-
-    # Keep only data for the given years while maintaining priority order
-    stacked_pwv = pd.concat(
-        [input_data[input_data.index.year == yr] for yr in years]
-    )
-
-    # Make all dates have the same year and keep only unique dates
-    stacked_pwv.index = [date_idx.replace(year=years[0]) for date_idx in stacked_pwv.index]
-    return stacked_pwv[~stacked_pwv.index.duplicated(keep='first')]
-
-
-def periodic_interpolation(series: pd.Series) -> pd.Series:
-    """Similar to linear interpolation on a pandas array, but missing values
-    at the beginning and end of the series are interpolated assuming a periodic
-    boundary condition.
-
-    Args:
-        series: A Pandas Series to infill by linear interpolation
-
-    Returns:
-        An interpolated copy of the passed series
-    """
-
-    if series.dtype is np.dtype('O'):
-        warnings.warn('Interpolation may not work for object data types', RuntimeWarning)
-
-    # Identify non-NAN values closest to the edges of the series
-    series = series.sort_index()
-    delta = series.index[1] - series.index[0]
-    start_idx, end_idx = series.iloc[[0, -1]].index
-    first_not_nan, last_not_nan = series.dropna().iloc[[0, -1]]
-
-    # Extend the series with temporary values so we can interpolate any missing values
-    series.loc[start_idx - 2 * delta] = last_not_nan
-    series.loc[start_idx - delta] = np.NAN
-    series.loc[end_idx + delta] = np.NAN
-    series.loc[end_idx + 2 * delta] = first_not_nan
-
-    # Drop the temporary values
-    return series.sort_index().interpolate().truncate(start_idx, end_idx)
-
-
-def resample_data_across_year(series: pd.Series) -> pd.Series:
-    """Return a copy of a pandas series resampled evenly from the
-    beginning of the earliest year through the end of the latest year.
-
-    Args:
-        series: A series with a Datetime index
-
-    Returns:
-        A copy of the passed series interpolated for January first through December 31st
-    """
-
-    start_time = series.index[0].replace(month=1, day=1, hour=0, minute=0, second=0)
-    end_time = series.index[-1].replace(month=12, day=31, hour=23, minute=59, second=59)
-    delta = series.index[1] - series.index[0]
-
-    # Modulo operation to determine any linear offset in the temporal sampling
-    offset = series.index[0] - start_time
-    while offset >= delta:
-        offset -= delta
-
-    index_values = np.arange(start_time, end_time, delta).astype(pd.Timestamp) + offset
-    new_index = pd.to_datetime(index_values).tz_localize(series.index.tz)
-    return series.reindex(new_index)
-
-
 @np.vectorize
-def datetime_to_season(time: Union[datetime, Collection[datetime]]) -> np.ndarray:
+def datetime_to_season(date: Union[dt.datetime, Collection[dt.datetime]]) -> np.ndarray:
     """Determine the calendar season corresponding to a given datetime
 
     Seasons are labeled as 'winter', 'spring', 'summer', or 'fall'.
 
     Args:
-        time (Datetime, List[Datetime]): Datetime value(s)
+        date: Datetime value(s)
 
     Returns:
         An array of strings
@@ -166,12 +64,105 @@ def datetime_to_season(time: Union[datetime, Collection[datetime]]) -> np.ndarra
 
     dummy_year = 2000  # dummy leap year to allow input X-02-29 (leap day)
     seasons = [
-        ('winter', (date(dummy_year, 1, 1), date(dummy_year, 3, 20))),
-        ('spring', (date(dummy_year, 3, 20), date(dummy_year, 6, 20))),
-        ('summer', (date(dummy_year, 6, 20), date(dummy_year, 9, 22))),
-        ('fall', (date(dummy_year, 9, 22), date(dummy_year, 12, 20))),
-        ('winter', (date(dummy_year, 12, 20), date(dummy_year + 1, 1, 1)))
+        ('winter', (dt.date(dummy_year, 1, 1), dt.date(dummy_year, 3, 20))),
+        ('spring', (dt.date(dummy_year, 3, 20), dt.date(dummy_year, 6, 20))),
+        ('summer', (dt.date(dummy_year, 6, 20), dt.date(dummy_year, 9, 22))),
+        ('fall', (dt.date(dummy_year, 9, 22), dt.date(dummy_year, 12, 20))),
+        ('winter', (dt.date(dummy_year, 12, 20), dt.date(dummy_year + 1, 1, 1)))
     ]
 
-    time = time.date().replace(year=dummy_year)
-    return cast(np.ndarray, next(season for season, (start, end) in seasons if start <= time < end))
+    date = date.date().replace(year=dummy_year)
+    return cast(np.ndarray, next(season for season, (start, end) in seasons if start <= date < end))
+
+
+@pd.api.extensions.register_series_accessor('snat_sim.tsu')
+class TSUAccessor:
+    """Pandas Series accessor for time series utilities"""
+
+    def __init__(self, pandas_obj: pd.Series) -> None:
+        self._validate(pandas_obj)
+        self._obj = pandas_obj
+
+    def supplemented_data(self, year: int, supp_years: Collection[int] = tuple()) -> pd.Series:
+        """Return the supplemented subset of the series corresponding to a given year
+
+        Data for the given year is supplemented with any available data from
+        supplementary years by asserting that the measured values from
+        supplementary years are exactly the same as they would be if taken during
+        the primary year. Priority is given to supplementary years in the order
+        specified by the ``supp_years`` argument.
+
+        Args:
+            year: Year to supplement data for
+            supp_years: Years to supplement data with when missing from ``year``
+
+        Returns:
+            A pandas Series object
+        """
+
+        input_data = self._obj.dropna().sort_index()
+        years = np.array([year, *supp_years])
+
+        # Check for years with no available data
+        missing_years = years[~np.isin(years, input_data.index.year)]
+        if missing_years:
+            raise ValueError(f'No data for years: {missing_years}')
+
+        # Keep only data for the given years while maintaining priority order
+        stacked_pwv = pd.concat(
+            [input_data[input_data.index.year == yr] for yr in years]
+        )
+
+        # Make all dates have the same year and keep only unique dates
+        stacked_pwv.index = [date_idx.replace(year=years[0]) for date_idx in stacked_pwv.index]
+        return stacked_pwv[~stacked_pwv.index.duplicated(keep='first')]
+
+    def periodic_interpolation(self) -> pd.Series:
+        """Linearly interpolate the series using periodic boundary conditions
+
+        Similar to the default linear interpolation used by pandas, but
+        missing values at the beginning and end of the series are
+        interpolated assuming a periodic boundary condition.
+
+        Returns:
+            An interpolated copy of the passed series
+        """
+
+        if self._obj.dtype is np.dtype('O'):
+            warnings.warn('Interpolation may not work for object data types', RuntimeWarning)
+
+        # Identify non-NAN values closest to the edges of the series
+        series = self._obj.sort_index()
+        delta = series.index[1] - series.index[0]
+        start_idx, end_idx = series.iloc[[0, -1]].index
+        first_not_nan, last_not_nan = series.dropna().iloc[[0, -1]]
+
+        # Extend the series with temporary values so we can interpolate any missing values
+        series.loc[start_idx - 2 * delta] = last_not_nan
+        series.loc[start_idx - delta] = np.NAN
+        series.loc[end_idx + delta] = np.NAN
+        series.loc[end_idx + 2 * delta] = first_not_nan
+
+        # Drop the temporary values
+        return series.sort_index().interpolate().truncate(start_idx, end_idx)
+
+    def resample_data_across_year(self) -> pd.Series:
+        """Resample the series evenly from the beginning of the
+        earliest year through the end of the latest year.
+
+        Returns:
+            A copy of the passed series interpolated for January first through December 31st
+        """
+
+        start_time = self._obj.index[0].replace(month=1, day=1, hour=0, minute=0, second=0)
+        end_time = self._obj.index[-1].replace(month=12, day=31, hour=23, minute=59, second=59)
+        delta = self._obj.index[1] - self._obj.index[0]
+
+        # Modulo operation to determine any linear offset in the temporal sampling
+        offset = self._obj.index[0] - start_time
+        while offset >= delta:
+            offset -= delta
+
+        index_values = np.arange(start_time, end_time, delta).astype(pd.Timestamp) + offset
+        new_index = pd.to_datetime(index_values).tz_localize(self._obj.index.tz)
+        return self._obj.reindex(new_index)
