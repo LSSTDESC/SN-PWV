ce8ea30052be5758b6aeede471552a5d5368a666
Daniel <djperrefort@gmail.com>
2020-12-31 12:00:48 -0500

    Updates pipeline usage example

--- snat_sim/pipeline.py
+++ snat_sim/pipeline.py
@@ -7,21 +7,23 @@ Usage Example
 
 Instances of the ``FittingPipeline`` class can be run synchronously
 (by calling ``FittingPipeline.run``) or asynchronously (with
-``FittingPipeline.run_async``). Here we demonstrate running a pipeline
-synchronously.
+``FittingPipeline.run_async``). A pipeline instance can be created as
+follows:
 
 .. doctest:: python
 
+   >>> from snat_sim.models import SNModel
    >>> from snat_sim.pipeline import FittingPipeline
 
    >>> pipeline = FittingPipeline(
-   >>>     cadence='alt_sched',
-   >>>     sim_model=SNModel('salt2'),
-   >>>     fit_model=SNModel('salt2'),
-   >>>     vparams=['x0', 'x1', 'c'],
-   >>>     out_path='./demo_out_path.csv',
-   >>>     pool_size=6
-   >>> )
+   ...     cadence='alt_sched',
+   ...     sim_model=SNModel('salt2'),
+   ...     fit_model=SNModel('salt2'),
+   ...     vparams=['x0', 'x1', 'c'],
+   ...     out_path='./demo_out_path.csv',
+   ...     fitting_pool=6,
+   ...     simulation_pool=3
+   ... )
 
 Module Docs
 -----------
