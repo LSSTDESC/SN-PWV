d5705f896297e8c21a4cea436b40d903bca7fb58
Daniel Perrefort <djperrefort@pitt.edu>
2021-01-12 10:45:04 -0500

    Runs new validation fits

--- scripts/validation/validation_fits.sh
+++ scripts/validation/validation_fits.sh
@@ -1,5 +1,5 @@
-python ../fitting_cli.py -s 2 -f 10 -i 115 -c alt_sched --sim_variability 0     --fit_variability 0        -d pwv_sim_0_fit_0 -o pwv_sim_0_fit_0.csv
-python ../fitting_cli.py -s 2 -f 10 -i 115 -c alt_sched --sim_variability 4     --fit_variability 4        -d pwv_sim_4_fit_4 -o pwv_sim_4_fit_4.csv
-python ../fitting_cli.py -s 2 -f 10 -i 115 -c alt_sched --sim_variability epoch --fit_variability 4        -d pwv_sim_epoch_fit_4 -o pwv_sim_epoch_fit_4.csv
-python ../fitting_cli.py -s 2 -f 10 -i 115 -c alt_sched --sim_variability epoch --fit_variability seasonal -d pwv_sim_epoch_fit_seasonal -o pwv_sim_epoch_fit_seasonal.csv
-python ../fitting_cli.py -s 2 -f 10 -i 115 -c alt_sched --sim_variability epoch --fit_variability epoch    -d pwv_sim_epoch_fit_epoch -o pwv_sim_epoch_fit_epoch.csv
+python ../fitting_cli.py -s 1 -f 10 -i 115 -c alt_sched --sim_variability 0     --fit_variability 0        -d pwv_sim_0_fit_0 -o pwv_sim_0_fit_0.csv
+python ../fitting_cli.py -s 1 -f 10 -i 115 -c alt_sched --sim_variability 4     --fit_variability 4        -d pwv_sim_4_fit_4 -o pwv_sim_4_fit_4.csv
+python ../fitting_cli.py -s 1 -f 10 -i 115 -c alt_sched --sim_variability epoch --fit_variability 4        -d pwv_sim_epoch_fit_4 -o pwv_sim_epoch_fit_4.csv
+python ../fitting_cli.py -s 1 -f 10 -i 115 -c alt_sched --sim_variability epoch --fit_variability seasonal -d pwv_sim_epoch_fit_seasonal -o pwv_sim_epoch_fit_seasonal.csv
+python ../fitting_cli.py -s 1 -f 10 -i 115 -c alt_sched --sim_variability epoch --fit_variability epoch    -d pwv_sim_epoch_fit_epoch -o pwv_sim_epoch_fit_epoch.csv
