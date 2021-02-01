4acc4fdef502c8e3d2ef879f9ece81e78ce59230
Daniel Perrefort <djperrefort@pitt.edu>
2021-02-01 16:00:02 -0500

    Adds pointers to data files in setup.py (#138)

--- setup.py
+++ setup.py
@@ -1,26 +1,41 @@
+import os
 import re
 from pathlib import Path
 
 from setuptools import find_packages, setup
 
-# Get list of requirements
+root_dir = Path(__file__).resolve().parent
+data_dir = root_dir / 'data'
+init_path = root_dir / 'snat_sim/__init__.py'
+
+# Get list of package requirements
 with open('requirements.txt') as f:
     core_requirements = f.read().splitlines()
 
+# Extra requirements for building docs
 with open('docs/requirements.txt') as f:
     doc_requirements = f.read().splitlines()
 
 # Get package version
-init_path = Path(__file__).resolve().parent / 'snat_sim/__init__.py'
 with init_path.open('r') as f:
-    source = f.read()
+    __version__ = re.compile("__version__ = '(.*?)'").findall(f.read())[0]
 
-versionRegExp = re.compile("__version__ = '(.*?)'")
-__version__ = versionRegExp.findall(source)[0]
+
+def gen_data_files(*dirs):
+    """Create a list of data files to include with the package"""
+
+    results = []
+    for src_dir in dirs:
+        for root, dirs, files in os.walk(src_dir):
+            results.append((root, map(lambda f: root + '/' + f, files)))
+
+    return results
+
 
 setup(name='snat_sim',
       version=__version__,
-      packages=find_packages(),
+      packages=find_packages() + ['data'],
+      data_files=gen_data_files('data'),
       keywords='Supernova Atmosphere Chromatic Effects',
       description='Simulation tools for atmospheric effects in photometric supernova observations.',
       author='The Dark Energy Science Collaboration (DESC)',
