32dbf6c09155726e75b66af3ed5d67450d6ddf23
Daniel Perrefort <djperrefort@pitt.edu>
2021-02-19 22:00:34 -0500

    Additional profiling results for core allotment

--- scripts/profiling/visualize_core_allocation.py
+++ scripts/profiling/visualize_core_allocation.py
@@ -25,14 +25,16 @@ for batch_of_profiling_runs in results_dir.glob('*'):
     profiling_data.append(run_times_for_batch)
 
 plot_data = pd.DataFrame(profiling_data)
-mean = plot_data.mean()
+reference_val = plot_data[min(plot_data.columns)]
+plot_data = ((plot_data.transpose() - reference_val) / reference_val).transpose()
+median = plot_data.median()
 std = plot_data.std()
 
 fig, axis = plt.subplots()
-axis.scatter(mean.index, mean)
-axis.errorbar(mean.index, mean, yerr=std, linestyle='')
+axis.scatter(median.index, median)
+axis.errorbar(median.index, median, yerr=std, linestyle='')
 
-xmin, xmax = mean.index.min() - 2, mean.index.max() + 2
+xmin, xmax = median.index.min() - 2, median.index.max() + 2
 axis.set_xlim(xmin, xmax)
 axis.set_xlabel('Forked Fitting Processes')
 axis.xaxis.set_major_locator(ticker.MultipleLocator(2))
@@ -44,5 +46,5 @@ twiny.set_xlabel('Forked Simulation Processes')
 twiny.xaxis.set_major_locator(ticker.MultipleLocator(2))
 twiny.xaxis.set_minor_locator(ticker.MultipleLocator(1))
 
-axis.set_ylabel('Runtime for 200 LCs (s)')
+axis.set_ylabel('Median runtime improvement for 250 LCs (%)')
 plt.show()
