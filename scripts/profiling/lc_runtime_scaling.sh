8278d1ab4ead35d42f2e4a0e5890f662d10b32ae
Daniel Perrefort <djperrefort@pitt.edu>
2021-02-22 17:55:24 -0500

    Adds job script for profiling scaling with number of lc

--- scripts/profiling/lc_runtime_scaling.sh
+++ scripts/profiling/lc_runtime_scaling.sh
@@ -6,24 +6,24 @@
 #SBATCH --qos=debug
 #SBATCH -L SCRATCH,cfs
 #SBATCH --constraint=haswell
-#SBATCH --array=42,46,50,54,58
+#SBATCH --array=400,800,1200,1600,2000
 
 module purge
 module load python
 
 conda activate SN-PWV
 
-# 64 cores total minus two processes for I/O
-export NON_IO_PROCESSES=62
+export NON_IO_PROCESSES=62 # 64 cores total minus two processes for I/O
+export FITTING_PROCESSES=52
 
 export OPENBLAS_NUM_THREADS=2
 export GOTO_NUM_THREADS=2
 export OMP_NUM_THREADS=2
 
-python -m cProfile -o "core_allocation_$SLURM_ARRAY_TASK_ID.pstat" ../fitting_cli.py \
+python -m cProfile -o "lc_runtime_scaling_$SLURM_ARRAY_TASK_ID.pstat" ../fitting_cli.py \
   --sim_pool_size $(($NON_IO_PROCESSES - $SLURM_ARRAY_TASK_ID)) \
-  --fit_pool_size $SLURM_ARRAY_TASK_ID \
-  --iter_lim 250 \
+  --fit_pool_size $FITTING_PROCESSES \
+  --iter_lim $SLURM_ARRAY_TASK_ID \
   --cadence alt_sched \
   --sim_variability epoch \
   --fit_variability seasonal \
