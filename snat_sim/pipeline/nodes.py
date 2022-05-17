fe64d99049a64d38824a87d95cb171856c6d6c3c
Daniel Perrefort <djperrefort@gmail.com>
2022-05-17 16:44:58 -0400

    Ensures analysis run light-curves are written to disk

--- snat_sim/pipeline/nodes.py
+++ snat_sim/pipeline/nodes.py
@@ -290,7 +290,7 @@ class WritePipelinePacket(Target):
         self._rotate_output_file()
 
         # We are taking the simulated parameters as guaranteed to exist
-        self.file_store.append('simulation/params', packet.sim_params_to_pandas())
+        self.file_store.append('simulation/params', packet.sim_params_to_pandas(), min_itemsize={'snid': 10})
         self.file_store.append('message', packet.packet_status_to_pandas().astype(str), min_itemsize={'snid': 10, 'message': 250})
 
         if self.write_lc_sims and packet.light_curve is not None:
