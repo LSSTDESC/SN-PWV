fae21c7ad5b97516424fc8282221f8a7c63b2e10
Daniel Perrefort <djperrefort@gmail.com>
2022-09-08 14:11:58 -0400

    Update profiling script to auto plot results

--- scripts/profiling/run_profiling.sh
+++ scripts/profiling/run_profiling.sh
@@ -1,20 +1,25 @@
-# Pipeline run for an entire cadence
+# Run the analysis pipeline with a varying number of processes
+# allocated to each pipeline node and plot the runtime.
 
 TOTAL_PROC=63
 MIN_ALLOC=2
 MAX_WRITE=5
 NUM_LC_SIMS=10000
 
+echo "Sim Processes | Fit Processes | Writing Processes"
+
+# Iterate over the number of writing processes to use
 for ((NUM_WRITE = 1; NUM_WRITE <= MAX_WRITE; NUM_WRITE++)); do
   REMAINING_PROC=$((TOTAL_PROC - NUM_WRITE - MIN_ALLOC))
 
+  # Iterate over the number of fitting proceses to use
   for ((NUM_FIT = MIN_ALLOC; NUM_FIT <= REMAINING_PROC; NUM_FIT += 2)); do
     NUM_SIM=$((TOTAL_PROC - NUM_WRITE - NUM_FIT))
-    
-    echo $NUM_SIM $NUM_FIT $NUM_WRITE
 
+    echo "$NUM_SIM | $NUM_FIT | $NUM_WRITE"
     rm -rf ./temp.*.h5
-    { time python ../../snat_sim_cli.py \
+
+    { time snat-sim \
         -i $NUM_LC_SIMS \
         -s $NUM_SIM \
         -f $NUM_FIT \
@@ -25,3 +30,5 @@ for ((NUM_WRITE = 1; NUM_WRITE <= MAX_WRITE; NUM_WRITE++)); do
         -o ./temp.h5 ; } 2>  "../../data/profiling/$NUM_SIM.$NUM_FIT.$NUM_WRITE.txt"
   done
 done
+
+python plot_results.py
