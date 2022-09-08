700d2bb9820eaa4760626e879d956d4476258ede
Daniel Perrefort <djperrefort@gmail.com>
2022-09-08 13:36:07 -0400

    Minor version bump

--- snat_sim/__init__.py
+++ snat_sim/__init__.py
@@ -10,7 +10,7 @@ from tables import NaturalNameWarning
 
 from .utils import setup_environment as _setup_environment
 
-__version__ = '0.0.0'
+__version__ = '0.1.0'
 __author__ = 'Dark Energy Science Collaboration'
 __maintainer__ = 'Daniel Perrefort'
 __license__ = 'GPL 3.0'
